03a88ecadfc1aa869834a506ca619653a37d2025
Hironaka <j870614@gmial.com>
2025-06-13 17:46:34 +0800

    除錯：環境變數設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // server-only
+    LINE_ACCESS_TOKEN: '',
+    LINE_CHANNEL_SECRET: '',
+    SUPABASE_SERVICE_ROLE_KEY: '',
+    public: {
+      SUPABASE_URL: ''
+    }
+  }
 })
+
